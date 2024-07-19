eba6f9f7d99f6eb1b23a9e708c4285b3a30ed00e
Emil Rohlen <emil.rohlen@live.se>
2024-07-20 01:02:21 +0200

    start of inventory renderer

--- Scripts/IngredientNode.cs
+++ Scripts/IngredientNode.cs
@@ -1,10 +1,9 @@
 using Godot;
 
-public partial class IngredientNode : Node2D {
+public partial class IngredientNode : Sprite2D {
     [Export] public IngredientData data;
-    public Sprite2D sprite;
 
     public override void _Ready () {
-        sprite.Texture = data.Texture;
+        Texture = data.Texture;
     }
 }
--- Scripts/Inventory.cs
+++ Scripts/Inventory.cs
@@ -7,18 +7,20 @@ public partial class Inventory : Node2D {
     public Array<PotionData> potions = new();
 
     public override void _Ready () {
+        PackedScene packedScene = ResourceLoader.Load<PackedScene>("Prefabs/Ingredient.tscn");
+        var node = packedScene.Instantiate<IngredientNode>();
+
+        node.data = ResourceLoader.Load<IngredientData>("Items/Ingredients/Critters/Red.tres");
+
+        CallDeferred("Spawn", node);
     }
 
     public override void _Process (double delta) {
-        IngredientNode node = null;
+        
+    }
 
-        if(Input.IsActionJustPressed("enter")) {
-            PackedScene packedScene = GD.Load<PackedScene>("res://Prefabs/Ingredient.tscn");
-            node = packedScene.Instantiate<IngredientNode>();
-
-        }
-
-        node?.sprite.Rotate(0.05f);
+    private void Spawn(Node node) {
+        GetTree().Root.AddChild(node);
     }
 
     public void ToggleDisplay() {
