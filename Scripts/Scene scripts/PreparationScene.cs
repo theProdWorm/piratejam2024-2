fe783c66dfad474394df31d6a3695338e4cb07fc
pixl00 <dennis.loov@gmail.com>
2024-07-20 01:22:00 +0200

    Scene switching and inventory persistence
    Inventory is now a "Global" that persists outside of the scene

---
+++ Scripts/Scene scripts/PreparationScene.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class PreparationScene : Node2D
+{
+    [Export] string CuttingScenePath = "res://Scenes/CuttingScene.tscn";
+
+    Inventory Inventory;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+
+        Inventory = GetNode<Inventory>( "/root/GlobalInventory" );
+		Ingredient ing = new Ingredient();
+		ing.Name = "Yes";
+		Inventory.ingredients.Add( ing );
+
+		PrintOrphanNodes();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        
+    }
+
+    public override void _UnhandledInput( InputEvent @event )
+    {
+    }
+
+	public void CuttingClicked()
+	{
+        Node cuttingScene = ResourceLoader.Load<PackedScene>( CuttingScenePath ).Instantiate();
+
+        GetTree().Root.AddChild( cuttingScene );
+        QueueFree();
+    }
+}
